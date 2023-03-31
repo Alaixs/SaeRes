38238920a1d66d1e08961f7e64a69196013a9047
Alexis <63883086+Alaixs@users.noreply.github.com>
2023-04-01 00:30:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,6 +18,7 @@ unzip main.zip
 sudo mv html /var/www/
 sudo mv SaeRes-main/intranet.conf /etc/apache2/sites-available/
 sudo mv SaeRes-main/public.conf /etc/apache2/sites-available/
+sudo mv SaeRes-main/000-default.conf /etc/apache2/sites-available/
 sudo mv SaeRes-main/srv ../../
 
 #on ajoute dans le fichier host les redirections url
