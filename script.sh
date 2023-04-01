cbd9e091175c7fcbce2ba446e0971795b8ef32c4
Alexis <63883086+Alaixs@users.noreply.github.com>
2023-04-01 12:04:30 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,8 +41,8 @@ sudo chmod -R u+rwx /var/www/html/ytanguy
 sudo chmod -R u+rwx /var/www/html/scody
 
 #on enleve les placeholder
-sudo rm /srv/www/public/testFolder/empytFolder/placeholder
-sudo rm /srv/intranet/testFolder/empytFolder/placeholder
+sudo rm /srv/www/public/testFolder/emptyFolder/placeholder
+sudo rm /srv/intranet/testFolder/emptyFolder/placeholder
 sudo rm /srv/www/public/log/acces/placeholder
 sudo rm /srv/www/public/log/erreur/placeholder
 sudo rm /srv/intranet/log/acces/placeholder
